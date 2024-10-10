a54dedc8780402e704784b5308542d5d1abee9fd
ACHii <>
2024-10-10 21:04:56 +0530

    Day 18

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,6 +3,10 @@
         Job
     </x-slot:heading>
 
-    <h1 class="font-bold text-lg">{{ $job['title'] }}</h1>
-    <p>This job pays {{ $job['salary'] }} per year.</p>
+    <h1 class="font-bold text-lg">{{ $job->title }}</h1>
+    <p>This job pays {{ $job->salary }} per year.</p>
+
+    <div class="mt-4">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+    </div>
 </x-layout>
