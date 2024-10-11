2b237168d241f936c0a02c3a8742abd2f505fc01
ACHii <>
2024-10-11 13:54:41 +0530

    Day 23

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -6,7 +6,9 @@
     <h1 class="font-bold text-lg">{{ $job->title }}</h1>
     <p>This job pays {{ $job->salary }} per year.</p>
 
-    <div class="mt-4">
-        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
-    </div>
+    @can('update', $job)
+        <div class="mt-4">
+            <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+        </div>
+    @endcan
 </x-layout>
